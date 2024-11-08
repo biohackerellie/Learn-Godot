7a59deb9e38ed295df5cc60799fa1a2948ee780d
biohackerellie <epkerns@gmail.com>
2024-11-07 18:13:42 -0700

    complete tutorial

--- gun.gd
+++ gun.gd
@@ -12,4 +12,12 @@ func shoot():
 	const BULLET = preload("res://bullet.tscn")
 	var new_bullet = BULLET.instantiate()
 	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	
 	%ShootingPoint.add_child(new_bullet)
+	
+	
+	
+func _on_timer_timeout():
+	shoot()
+	
